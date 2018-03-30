a1b19e1d10896f3ac2ce0b97c2bf24e0c1c4520f
Elvis Pranskevichus <elvis@magic.io>
2018-03-30 19:47:52 -0400

    CI integration

---
+++ .ci/build-manylinux-wheels.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e -x
+
+# Compile wheels
+PYTHON="/opt/python/${PYTHON_VERSION}/bin/python"
+PIP="/opt/python/${PYTHON_VERSION}/bin/pip"
+${PIP} install --upgrade pip wheel
+${PIP} install --upgrade setuptools
+${PIP} install -r /io/.ci/requirements.txt
+make -C /io/ PYTHON="${PYTHON}"
+${PIP} wheel /io/ -w /io/dist/
+
+# Bundle external shared libraries into the wheels.
+for whl in /io/dist/*.whl; do
+    auditwheel repair $whl -w /io/dist/
+    rm /io/dist/*-linux_*.whl
+done
+
+# Grab docker host, where Postgres should be running.
+export PGHOST=$(ip route | awk '/default/ { print $3 }' | uniq)
+export PGUSER="postgres"
+
+PYTHON="/opt/python/${PYTHON_VERSION}/bin/python"
+PIP="/opt/python/${PYTHON_VERSION}/bin/pip"
+${PIP} install ${PYMODULE} --no-index -f file:///io/dist
+rm -rf /io/tests/__pycache__
+make -C /io/ PYTHON="${PYTHON}" testinstalled
+rm -rf /io/tests/__pycache__
---
+++ .ci/travis-before-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -x
+
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    if ! (pyenv versions | grep "${PYTHON_VERSION}$"); then
+        pyenv install ${PYTHON_VERSION}
+    fi
+    pyenv global ${PYTHON_VERSION}
+    pyenv rehash
+fi
