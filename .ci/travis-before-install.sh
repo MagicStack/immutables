cc6a62c9dc94a4cd67f4c909699886cfecd16303
Elvis Pranskevichus <elvis@magic.io>
2018-05-01 12:18:48 -0400

    Unbreak macOS build on Travis

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -3,14 +3,14 @@
 set -e -x
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-    git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
-    PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
+    brew update >/dev/null
+    brew upgrade pyenv
     eval "$(pyenv init -)"
 
     if ! (pyenv versions | grep "${PYTHON_VERSION}$"); then
         pyenv install ${PYTHON_VERSION}
     fi
+
     pyenv global ${PYTHON_VERSION}
     pyenv rehash
 fi
