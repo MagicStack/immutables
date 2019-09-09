025d7a971a142006e8f7d47d167b04fbb520591b
Anton Agestam <git@antonagestam.se>
2019-09-09 20:55:52 +0200

    fix: skip subscriptability test for python<3.7

--- tests/test_map.py
+++ tests/test_map.py
@@ -2,6 +2,7 @@ import collections.abc
 import gc
 import pickle
 import random
+import sys
 import unittest
 import weakref
 
@@ -1296,6 +1297,7 @@ class BaseMapTest:
         with self.assertRaisesRegex(TypeError, "can't pickle"):
             pickle.dumps(h.mutate())
 
+    @unittest.skipIf(sys.version_info < (3, 7, 0), "__class_getitem__ is not available")
     def test_map_is_subscriptable(self):
         self.assertIs(self.Map[int, str], self.Map)
 
