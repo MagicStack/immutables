7edf30466c451a16e2d671f792967267d61dc211
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-10-08 15:19:35 +0300

    Test Python 3.9

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
