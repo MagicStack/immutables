0ffa902851b6b68f05593292931bdeae0c06e4d7
Elvis Pranskevichus <elvis@edgedb.com>
2021-08-05 09:05:05 -0700

    Use cibuildwheel to build wheels
    The cibuildwheel infra seems to be mature enough, so use it for the
    wheel-building step instead of the homegrown setup.  This gives us
    32-bit and ARM wheels for free.
    
    Fixes: #44
    Fixes: #63

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -1,23 +1,26 @@
 import os
-import mypy.test.testcmdline
-from mypy.test.helpers import normalize_error_messages
 
+try:
+    import mypy.test.testcmdline
+    from mypy.test.helpers import normalize_error_messages
+except (ImportError, AssertionError):
+    if os.environ.get('IMMU_SKIP_MYPY_TESTS'):
+        pass
+    else:
+        raise
+else:
+    # I'm upset. There's no other way to deal with the little 'defined here'
+    # notes that mypy emits when passing an unexpected keyword argument
+    # and at no other time.
+    def renormalize_error_messages(messages):
+        messages = [x for x in messages if not x.endswith(' defined here')]
+        return normalize_error_messages(messages)
 
-# I'm upset. There's no other way to deal with the little 'defined here'
-# notes that mypy emits when passing an unexpected keyword argument
-# and at no other time.
-def renormalize_error_messages(messages):
-    messages = [x for x in messages if not x.endswith(' defined here')]
-    return normalize_error_messages(messages)
+    mypy.test.testcmdline.normalize_error_messages = renormalize_error_messages
 
+    this_file_dir = os.path.dirname(os.path.realpath(__file__))
+    test_data_prefix = os.path.join(this_file_dir, 'test-data')
 
-mypy.test.testcmdline.normalize_error_messages = renormalize_error_messages
-
-
-this_file_dir = os.path.dirname(os.path.realpath(__file__))
-test_data_prefix = os.path.join(this_file_dir, 'test-data')
-
-
-class ImmuMypyTest(mypy.test.testcmdline.PythonCmdlineSuite):
-    data_prefix = test_data_prefix
-    files = ['check-immu.test']
+    class ImmuMypyTest(mypy.test.testcmdline.PythonCmdlineSuite):
+        data_prefix = test_data_prefix
+        files = ['check-immu.test']
