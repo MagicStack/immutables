ea6565672a5634270b149129f1b4206b899160d4
Elvis Pranskevichus <elvis@edgedb.com>
2023-08-14 15:49:50 -0700

    Move metadata to `pyproject.toml`, fix test fixture inclusion in sdist (#111)
    Fixes: #109

--- setup.py
+++ setup.py
@@ -3,21 +3,6 @@ import platform
 import setuptools
 
 
-# Minimal dependencies required to test immutables.
-TEST_DEPENDENCIES = [
-    # pycodestyle is a dependency of flake8, but it must be frozen because
-    # their combination breaks too often
-    # (example breakage: https://gitlab.com/pycqa/flake8/issues/427)
-    'flake8~=5.0',
-    'pycodestyle~=2.9',
-    'mypy~=1.4',
-    'pytest~=7.4',
-]
-
-EXTRA_DEPENDENCIES = {
-    'test': TEST_DEPENDENCIES,
-}
-
 system = platform.uname().system
 CFLAGS = ['-O2']
 
@@ -62,38 +47,7 @@ else:
     ext_modules = []
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
-    readme = f.read()
-
-
 setuptools.setup(
-    name='immutables',
     version=VERSION,
-    description='Immutable Collections',
-    long_description=readme,
-    python_requires='>=3.7',
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-    ],
-    author='MagicStack Inc',
-    author_email='hello@magic.io',
-    url='https://github.com/MagicStack/immutables',
-    license='Apache License, Version 2.0',
-    packages=['immutables'],
-    package_data={"immutables": ["py.typed", "*.pyi"]},
-    provides=['immutables'],
-    include_package_data=True,
     ext_modules=ext_modules,
-    install_requires=['typing-extensions>=3.7.4.3;python_version<"3.8"'],
-    extras_require=EXTRA_DEPENDENCIES,
 )
